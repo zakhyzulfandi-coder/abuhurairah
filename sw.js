598b8b553643f3b706087e82fa7cd7e1174268b1
zakhyzulfandi-coder <zakhyzulfandi@gmail.com>
2026-06-18 21:27:52 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,88 @@
+const CACHE_NAME = "sisfo-ahis-pwa-cache-v2026061802";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./offline.html",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(APP_SHELL);
+      })
+      .then(function () {
+        return self.skipWaiting();
+      })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys()
+      .then(function (keys) {
+        return Promise.all(
+          keys
+            .filter(function (key) {
+              return key !== CACHE_NAME;
+            })
+            .map(function (key) {
+              return caches.delete(key);
+            })
+        );
+      })
+      .then(function () {
+        return self.clients.claim();
+      })
+  );
+});
+
+self.addEventListener("message", function (event) {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("fetch", function (event) {
+  const request = event.request;
+  const url = new URL(request.url);
+
+  if (request.method !== "GET") return;
+
+  if (url.origin === self.location.origin) {
+    if (request.mode === "navigate" || url.pathname.endsWith(".html")) {
+      event.respondWith(
+        fetch(request)
+          .then(function (response) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then(function (cache) {
+              cache.put(request, copy);
+            });
+            return response;
+          })
+          .catch(function () {
+            return caches.match(request).then(function (cached) {
+              return cached || caches.match("./offline.html");
+            });
+          })
+      );
+      return;
+    }
+
+    event.respondWith(
+      caches.match(request)
+        .then(function (cached) {
+          return cached || fetch(request).then(function (response) {
+            const copy = response.clone();
+            caches.open(CACHE_NAME).then(function (cache) {
+              cache.put(request, copy);
+            });
+            return response;
+          });
+        })
+    );
+  }
+});
